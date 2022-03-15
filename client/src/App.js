4f64a5d7d0d7c6d54b7f9b3c0ad8e7cc76bec00a
Lampros Papantoniou <95181559+LamprosPapantoniou@users.noreply.github.com>
2022-03-15 21:38:01 +0200

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React, { Fragment } from 'react';
+import './App.css';
+import { Container } from '@material-ui/core';
+import { BrowserRouter as Router, Switch, Route, Routes } from "react-router-dom";
+
+
+//components
+
+import InputEmployee from './components/InputEmployee';
+import ListEmployees from './components/ListEmployees';
+import EditEmployee from './components/EditEmployee';
+import ButtonAppBar from './components/Navbar';
+
+
+function App() {
+  return (
+    <Router>
+    <Fragment>
+      <Container fixed>
+      <ButtonAppBar />
+      <Routes>
+      <Route index path="/" element={<ListEmployees/> }  />
+      <Route path="/employees/new" element={<InputEmployee />} />
+      <Route path="/employees/:id/edit" element={<EditEmployee />} />
+      </Routes>
+      </Container>
+    </Fragment>
+    </Router>
+  );
+}
+
+export default App;
