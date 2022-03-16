f9f86710f4fc9d06b22664985a3332e62b924c36
Lampros Papantoniou <95181559+LamprosPapantoniou@users.noreply.github.com>
2022-03-16 13:09:32 +0200

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import './App.css';
 import { Container } from '@material-ui/core';
-import { BrowserRouter as Router, Switch, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 
 //components
