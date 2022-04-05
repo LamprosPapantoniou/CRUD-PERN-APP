0d5bdb007bc897f9dd65459a9ea78b44ec0abb28
Lampros Papantoniou <95181559+LamprosPapantoniou@users.noreply.github.com>
2022-04-05 15:21:57 +0300

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -10,11 +10,14 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json()); //req.body
 
+//
+if (process.env.NODE_ENV === 'production'){
 app.use(express.static(path.join(__dirname + "/publc")))
+}
 
 app.listen(PORT, () => {
     console.log(`server started on port ${PORT}`);
-  });
+});
 
 
 //ROUTES//
