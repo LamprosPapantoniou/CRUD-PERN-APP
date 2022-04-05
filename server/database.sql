c9fc24961957bf80d70adef5f2d227cef5d21c9f
Lampros Papantoniou <95181559+LamprosPapantoniou@users.noreply.github.com>
2022-04-06 00:01:41 +0300

    Delete server directory

--- server/database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE company;
-
-CREATE TABLE employees(
-    id SERIAL PRIMARY KEY,
-    firstName VARCHAR(19),
-    lastName VARCHAR(25),
-    birthDate DATE,
-    afm VARCHAR(9) UNIQUE
-);
