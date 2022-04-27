9dc3948bd0e67ba0b3612b71b56e0bfc71807cab
Lampros Papantoniou <95181559+LamprosPapantoniou@users.noreply.github.com>
2022-04-27 16:21:28 +0300

    Add files via upload

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-const Pool = require('pg').Pool;
+const Pool = require("pg").Pool;
 require("dotenv").config();
 
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
@@ -8,10 +8,9 @@ const proConfig = process.env.DATABASE_URL; //heroku addons
 const pool = new Pool({
   connectionString:
     process.env.NODE_ENV === "production" ? proConfig : devConfig,
-   ssl: {
-    rejectUnauthorized: false
-  }
-  
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
